13d6122f61c6ba66d92465f95952b5a0b7af5e75
Vishakhasenthilnathan <Vishakhasenthilnathan@gmail.com>
2022-10-10 23:07:02 +0530

    Basic login Ui with Anonymous login

---
+++ app/src/main/java/com/project/uber/MainActivity.java
@@ -0,0 +1,49 @@
+package com.project.uber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Switch;
+
+import com.parse.LogInCallback;
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        if(ParseUser.getCurrentUser()==null){
+            ParseAnonymousUtils.logIn(new LogInCallback() {
+                @Override
+                public void done(ParseUser user, ParseException e) {
+                    if(e==null){
+                        Log.i("Login","Anonymous login");
+                    }else {
+                        Log.i("Login","Anonymous login failed");
+                    }
+                }
+            });
+        }
+
+    }
+    public void getStarted(View view){
+        Switch userSwitch = (Switch) findViewById(R.id.userSwitch);
+        Log.i("switch", String.valueOf(userSwitch.isChecked()));
+
+        String userType = "Rider";
+        if(userSwitch.isChecked()){
+            userType = "Driver";
+        }
+        ParseUser.getCurrentUser().put("riderOrDriver",userType);
+        Log.i("riderOrDriver", userType);
+    }
+}
